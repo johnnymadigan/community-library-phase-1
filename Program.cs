3441fe939675d1e4798dbce3e1d7f6536729e11c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-03 20:45:00 +1000

    Testing

--- Program.cs
+++ Program.cs
@@ -11,41 +11,38 @@ namespace MemberApp
 
 
             Member m1 = new Member("i go first", "a");
-            //Member m2 = new Member("i go last", "z");
-            //Member m3 = new Member("im in the middle", "madigan");
-            //Member m4 = new Member("im in the middle", "madigan");
-            //Member m5 = new Member("im in the middle i come after the others", "madigan");
-            //Member m6 = new Member("i go 2nd", "b");
-            //Member m7 = new Member("i go 3rd", "c");
-            //Member m8 = new Member("i go 4th", "d");
-            //Member m9 = new Member("he", "horndog");
-            //Member m10 = new Member("he", "dude");
-            //Member m11 = new Member("not", "added");
+            Member m2 = new Member("i go last", "z");
+            Member m3 = new Member("im in the middle", "madigan");
+            Member m4 = new Member("im in the middle", "madigan");
+            Member m5 = new Member("im in the middle i come after the others", "madigan");
+            Member m6 = new Member("i go 2nd", "b");
+            Member m7 = new Member("i go 3rd", "c");
+            Member m8 = new Member("i go 4th", "d");
+            Member m9 = new Member("he", "horndog");
+            Member m10 = new Member("he", "dude");
+            Member m11 = new Member("not", "added");
 
 
             col.Add(m1);
+            col.Add(m2);
+            col.Add(m3);
+            col.Add(m4);
+            col.Add(m5);
+            col.Add(m6);
+            col.Add(m7);
+            col.Add(m8);
+            col.Add(m9);
+            col.Add(m10);
+            col.Add(m11);
+
+
             Console.WriteLine(col.ToString());
-            Console.WriteLine("end");
 
-            //col.Add(m2);
-            //col.Add(m3);
-            //col.Add(m4);
-            //col.Add(m5);
-            //col.Add(m6);
-            //col.Add(m7);
-            //col.Add(m8);
-            //col.Add(m9);
-            //col.Add(m10);
-            //col.Add(m11);
-
-
-            //Console.WriteLine(col.ToString());
-
-            //Console.WriteLine(col.Search(m1));
-            //col.Delete(m1);
-
-            //Console.WriteLine(col.Search(m1));
-            //Console.WriteLine(col.ToString());
+            Console.WriteLine(col.Search(m1));
+            col.Delete(m1);
+            col.Delete(m1);
+            Console.WriteLine(col.Search(m1));
+            Console.WriteLine(col.ToString());
 
         }
     }
