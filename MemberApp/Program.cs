aa8876589d0b9f8c8d8cfaeef5227dba089c9a66
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-10 11:43:22 +1000

    Further testing

--- MemberApp/Program.cs
+++ MemberApp/Program.cs
@@ -6,21 +6,49 @@ namespace MemberApp
     {
         static void Main(string[] args)
         {
+            IMemberCollection col = new MemberCollection(5);
 
-            MemberCollection col = new MemberCollection(10);
+            IMember m1 = new Member("i go last", "z");
+            IMember m2 = new Member("nutz", "deez");
+            IMember m3 = new Member("i go first", "a");
+            IMember m4 = new Member("dem", "bofa");
+            IMember m5 = new Member("im in the middle", "middle");
 
-            Member m1 = new Member("i go first", "a");
-            Member m2 = new Member("i go last", "z");
-            Member m3 = new Member("im in the middle", "madigan");
-            
+            m1.IsValidPhoneNumber(m1);
+
+            col.Delete(m1); // empty
 
             col.Add(m1);
             col.Add(m2);
             col.Add(m3);
-            Console.WriteLine(col.ToString());
+            col.Add(m3);    // dupe
+            col.Add(m4);
+            col.Add(m5);
+            col.Add(m5);    // full (before recognising dupe)
 
+            Console.WriteLine(col.Search(m5)); // true
+
+            col.Delete(m1);
+            col.Delete(m2);
+
+            Console.WriteLine(col.Search(m2)); // false
+
+            col.Delete(m3);
+            col.Delete(m4);
+            col.Delete(m5);
+            col.Delete(m5); // does not exist
+
+            Console.WriteLine(col.Search(m5)); // false (quick cus empty)
+
+            col.Add(m1);
+            col.Add(m2);
+            Console.WriteLine(col.Search(m3)); // false (not empty but added yet)
             col.Add(m3);
-            Console.WriteLine(col.ToString());
+            col.Add(m4);
+            col.Add(m5);
+
+            Console.WriteLine("________________________________\n" +
+                col.ToString() + "________________________________\n");
         }
     }
 }
